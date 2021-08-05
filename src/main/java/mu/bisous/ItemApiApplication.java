6b402bcccb6230bf4698a7a48543e67f3b75c4d2
Yanesh Caussy <yanesh.caussy@tribalworldwide.co.uk>
2021-08-05 20:47:37 +0400

    Project initialise

---
+++ src/main/java/mu/bisous/ItemApiApplication.java
@@ -0,0 +1,13 @@
+package mu.bisous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mu/bisous/ItemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mu.bisous;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
